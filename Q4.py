c403d37ed7600dde81f7321a31762fb60a55374a
freshprinceofcoders <milanosolanki27@gmail.com>
2020-07-21 16:46:41 +0100

    done

--- Q4.py
+++ Q4.py
@@ -1,1 +1,9 @@
-
+iterations = 0 
+for x in range(999,100,-1):
+    for y in range(999,100,-1):
+        z = x * y
+        if z > iterations:
+            a = str(x*y)
+            if a == a[::-1]:
+                iterations = x * y 
+print(iterations)                
